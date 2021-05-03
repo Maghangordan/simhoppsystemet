30a3b12bcf8ae442d7581708a027da9db9707bfa
Intha <juncor94@gmail.com>
2021-05-03 09:55:54 +0200

    Changed to required on input of name, et on models to make sure input are correct to the DB

--- Models/Competition.cs
+++ Models/Competition.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace simhoppsystemet.Models
 {
     public class Competition
     {
         public int Id { get; set; }
-        public string Date { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         public Competition()
         {
--- Models/Competitor.cs
+++ Models/Competitor.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace simhoppsystemet.Models
 {
@@ -9,8 +11,14 @@ namespace simhoppsystemet.Models
     {
         public int Id { get; set; }
         public int Age { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Gender { get; set; } //String due to political reasons
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Organization { get; set; }
 
         public List<Dive> Dives = new List<Dive>();
