f626130a2757f4b4d6b91d0c14827c05f4df31d8
Intha <juncor94@gmail.com>
2021-05-12 22:55:23 +0200

    Fixed divegroups, better interface for judging, and also looks better while choosing which dive to make

---
+++ Controllers/DiveGroupsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using simhoppsystemet.Data;
+using simhoppsystemet.Models;
+
+namespace simhoppsystemet.Controllers
+{
+    public class DiveGroupsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiveGroupsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DiveGroups
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DiveGroup.ToListAsync());
+        }
+
+        // GET: DiveGroups/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diveGroup = await _context.DiveGroup
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diveGroup == null)
+            {
+                return NotFound();
+            }
+
+            return View(diveGroup);
+        }
+
+        // GET: DiveGroups/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DiveGroups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Dive,Difficulty")] DiveGroup diveGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(diveGroup);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diveGroup);
+        }
+
+        // GET: DiveGroups/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diveGroup = await _context.DiveGroup.FindAsync(id);
+            if (diveGroup == null)
+            {
+                return NotFound();
+            }
+            return View(diveGroup);
+        }
+
+        // POST: DiveGroups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Dive,Difficulty")] DiveGroup diveGroup)
+        {
+            if (id != diveGroup.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(diveGroup);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiveGroupExists(diveGroup.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diveGroup);
+        }
+
+        // GET: DiveGroups/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diveGroup = await _context.DiveGroup
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diveGroup == null)
+            {
+                return NotFound();
+            }
+
+            return View(diveGroup);
+        }
+
+        // POST: DiveGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var diveGroup = await _context.DiveGroup.FindAsync(id);
+            _context.DiveGroup.Remove(diveGroup);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiveGroupExists(int id)
+        {
+            return _context.DiveGroup.Any(e => e.Id == id);
+        }
+    }
+}
--- Controllers/DivesController.cs
+++ Controllers/DivesController.cs
@@ -87,6 +87,7 @@ namespace simhoppsystemet.Controllers
             }
             ViewData["CompetitionId"] = new SelectList(_context.Competition, "Id", "Id", dive.CompetitionId);
             ViewData["CompetitorId"] = new SelectList(_context.Competitor, "Id", "Id", dive.CompetitorId);
+            ViewData["divecategories"] = new SelectList(_context.DiveGroup, "Dive", "Dive", dive.DiveGroup);
             return View(dive);
         }
 
@@ -128,6 +129,69 @@ namespace simhoppsystemet.Controllers
             }
             ViewData["CompetitionId"] = new SelectList(_context.Competition, "Id", "Id", dive.CompetitionId);
             ViewData["CompetitorId"] = new SelectList(_context.Competitor, "Id", "Id", dive.CompetitorId);
+            ViewData["divecategories"] = new SelectList(_context.DiveGroup, "Dive", "Dive", dive.DiveGroup);
+
+            return View(dive);
+        }
+
+        // GET: Dives/Edit/5
+        public async Task<IActionResult> JudgeView(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dive = await _context.Dive.FindAsync(id);
+            if (dive == null)
+            {
+                return NotFound();
+            }
+            ViewData["CompetitionId"] = new SelectList(_context.Competition, "Id", "Id", dive.CompetitionId);
+            ViewData["CompetitorId"] = new SelectList(_context.Competitor, "Id", "Id", dive.CompetitorId);
+            ViewData["divecategories"] = new SelectList(_context.DiveGroup, "Dive", "Dive", dive.DiveGroup);
+            return View(dive);
+        }
+
+        // POST: Dives/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> JudgeView(int id, [Bind("Judge1,Judge2,Judge3")] Dive dive)
+        {
+            if (id != dive.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dive.Score = dive.Judge1 + dive.Judge2 + dive.Judge3;
+
+
+                    _context.Update(dive);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiveExists(dive.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction("JudgeDive", new { CompetitorId = dive.CompetitorId, CompetitionId = dive.CompetitionId });
+            }
+            ViewData["CompetitionId"] = new SelectList(_context.Competition, "Id", "Id", dive.CompetitionId);
+            ViewData["CompetitorId"] = new SelectList(_context.Competitor, "Id", "Id", dive.CompetitorId);
+            ViewData["divecategories"] = new SelectList(_context.DiveGroup, "Dive", "Dive", dive.DiveGroup);
 
             return View(dive);
         }
